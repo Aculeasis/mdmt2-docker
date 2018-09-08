5c83f867ba527510b1ac8c1c88b8bf793f2b34d8
aculeasis <42580940+Aculeasis@users.noreply.github.com>
2018-09-08 04:26:37 +0300

    fix, create files after mount

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,9 +15,25 @@ cleanup() {
 }
 
 if [ ! -f /opt/cfg/configured ]; then
+    touch /opt/cfg/asound.conf
+    ln -s /opt/cfg/asound.conf /etc/asound.conf
+
+    touch /opt/cfg/settings.ini
+    ln -s /opt/cfg/settings.ini /opt/mdmterminal2/settings.ini
+
+    if [ -f /usr/local/etc/RHVoice/RHVoice.conf ] && [ ! -L /usr/local/etc/RHVoice/RHVoice.conf ]; then
+        mv /usr/local/etc/RHVoice/RHVoice.conf /opt/cfg/RHVoice.conf
+        ln -s /opt/cfg/RHVoice.conf /usr/local/etc/RHVoice/RHVoice.conf
+    fi
+
+    if [ -f /opt/asound.conf.h3 ]; then
+        mv /opt/asound.conf.h3 /opt/cfg/asound.conf.h3
+    fi
+
     if [ -f /opt/mdmterminal2/crutch.py ]; then
         python3 -u /opt/mdmterminal2/crutch.py
     fi
+
     touch /opt/cfg/configured
 fi
 
